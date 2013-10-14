fa38685ef1843aeee241776938e943e30c61972e
Edward Chik <edward.chik@gmail.com>
2013-10-13 23:12:02 -0400

    Week 6 updates added

--- config/feature_flags.php
+++ config/feature_flags.php
@@ -2,3 +2,10 @@
 
 # Example:
 	//define('ENABLE_SOME_NEW_FEATURE', TRUE);
+
+/*
+define('ENABLE_PUBNUB', FALSE);
+define('ENABLE_ANALYTICS', TRUE);
+define('ENABLE_GEOLOCATION', TRUE);
+define('ENABLE_OLARK', TRUE);
+*/
