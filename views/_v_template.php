83abfa1f47759d71a1387a1c739138d5f1506150
Edward Chik <edward.chik@gmail.com>
2013-11-04 20:41:09 -0500

    Basic top navigation and font upgraded with Bootstrap

--- views/_v_template.php
+++ views/_v_template.php
@@ -7,36 +7,72 @@
 					
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
+
+    <!-- Twitter Bootstrap framework -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">    
 	
 </head>
 
 <body>  
 
     <div id='menu'>
+        <div>
+            <header>
+                <!-- navigation bar -->
+                <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+                    <!-- main site navigation bar -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
 
-        <a href='/'>Home</a>
+                            <!-- Menu for users who are logged in -->
+                            <?php if($user): ?>
 
-        <!-- Menu for users who are logged in -->
-        <?php if($user): ?>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            <?php endif; ?>
 
-            <a href='/users/logout'>Logout</a>
-            <a href='/users/profile'>Profile</a>
-            <a href='/posts'>Posts</a>
-            <a href='/posts/users'>Users</a>
+                        </button>
+                    </div>
 
-        <!-- Menu options for users who are not logged in -->
-        <?php else: ?>
+                    <div class="collapse navbar-collapse navbar-ex1-collapse">
+                        <ul class="nav navbar-nav">
 
-            <a href='/users/signup'>Sign up</a>
-            <a href='/users/login'>Log in</a>
-
-        <?php endif; ?>
-
+                            <!-- Menu for users who are logged in -->
+                            <?php if($user): ?>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/users/logout">Logout</a></li>
+                                <li><a href="/users/profile">Profile</a></li>
+                                <li><a href="/posts">Posts</a></li>
+                                <li><a href="/posts/users">Users</a></li>
+                            <!-- Menu options for users who are not logged in -->
+                            <?php else: ?>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/users/signup">Sign up</a></li>
+                                <li><a href="/users/login">Log in</a></li>
+                            <?php endif; ?>     
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </div>
     </div>
 
     <br>
 
     <?php if(isset($content)) echo $content; ?>
 
+
+    <!-- jQuery leveraged by Bootstrap for selected display elements -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <script type="text/javascript">
+        $('.show_tooltip').tooltip();
+    </script>
 </body>
 </html>
